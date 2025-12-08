fb0016cb93f7ee6d00f61487f5e9ba0513c23fee
Nob <otamasan959@gmail.com>
2025-12-08 19:30:21 +0900

    Reactで頻出のJavaScriptの記法

---
+++ 03_js_basic/030_esmodule/start/main.js
@@ -0,0 +1,6 @@
+import functionB, { hello, User } from "./module.js";
+
+functionB();
+hello();
+const user = new User("Tom");
+user.hello();
---
+++ 03_js_basic/045_eventlistener/start/main.js
@@ -0,0 +1,12 @@
+const h1Element = document.querySelector('h1');
+console.log(h1Element);
+
+console.log(h1Element.textContent);
+
+h1Element.textContent = "変更後のタイトル";
+
+const btnEl = document.querySelector('button');
+btnEl.addEventListener('click', (e) => {
+  console.dir(e.target.textContent);
+  console.log('hello');
+})
