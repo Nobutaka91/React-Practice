76cbb4db7c0b5f318411031b391a99e2ee86f7dd
Nob <otamasan959@gmail.com>
2025-12-25 20:52:03 +0900

    Functional Programming

--- 10_functional_programming/src/020_pure_function/start/Example.jsx
+++ 10_functional_programming/src/020_pure_function/start/Example.jsx
@@ -1,8 +1,14 @@
 const Example = () => {
+
+  const val1 = 2, val2 = 3;
+
+  const add = (val1) => {
+    return val1 + val2; // 関数の外部スコープで定義されたval2を参照しているため、これは純粋ではない
+  }
+
   return (
     <>
-      <h3>純粋関数</h3>
-      <p>fn(決まった引数) には 決まった戻り値 を返す</p>
+      <div>純粋関数: {add(val1)}</div>
     </>
   );
 };
